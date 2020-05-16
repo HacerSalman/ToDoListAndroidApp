b891080e7e6da70d882423e947ba0feffd3e5d09
Hacer <Hacer@DESKTOP-2JMK2LJ>
2020-05-16 23:37:47 +0300

    Fragment

--- app/src/main/java/com/example/todolistandroidapp/MainActivity.java
+++ app/src/main/java/com/example/todolistandroidapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.todolistandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            MainMenuFragment mainMenuFragmentFragment = new MainMenuFragment();
+            mainMenuFragmentFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment, mainMenuFragmentFragment).commit();
+        }
+    }
+
+    public void ReplaceFragment(Fragment fragment, Bundle args)
+    {
+        fragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/todolistandroidapp/ManageListFragment.java
@@ -0,0 +1,46 @@
+package com.example.todolistandroidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+
+public class ManageListFragment extends Fragment {
+
+
+    public ManageListFragment() {
+        // Required empty public constructor
+    }
+
+    public static ManageListFragment newInstance(String param1, String param2) {
+        ManageListFragment fragment = new ManageListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manage_list, container, false);
+    }
+
+
+}
